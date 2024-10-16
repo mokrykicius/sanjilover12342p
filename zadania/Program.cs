bccf3f2d3bfe5edc7c822f0518ceaa76c889c2d0
jilix <nagitosupermacy@gmail.com>
2024-10-16 12:44:06 +0200

    petlawhile

--- zadania/Program.cs
+++ zadania/Program.cs
@@ -1,10 +1,29 @@
-﻿class Program
+﻿using System.ComponentModel.Design;
+
+class Program
 {
     static void Main(string[] args)
     {
-        for (int i = 0; i < 10 i++) {
-            Console.WriteLine(i);
+        zad1();
+        zad2();
+      static void zad1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+
+            }
+        }
+        static void zad2() {
+                int i = 0;
+                
+                while (i < 6) 
+                {
+                    Console.WriteLine("wartosc mniejsza od 6");
+                    i++;
+                }
+            }
         }
     }
-}
+
 
